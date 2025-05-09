5c18529adcf43ffbae629c91e88cfcf48657fa18
crevmick <aworetanbidemi@gmail.com>
2025-05-09 23:57:34 +0100

    Feat: Database creation

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './Config/db.js';
 
 dotenv.config();
 
 //Initialling the application 
 const app = express();
+
+//connect DB 
+connectDB();
 
 //Listen to our server
 const PORT = process.env.PORT ||3000;
