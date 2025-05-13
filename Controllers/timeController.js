587cc7a6c6785260aa545db3ae5312638f54bddf
Espee <atari.m1802476@st.futminna.edu.ng>
2025-05-13 16:58:52 +0100

    Added minutes to other units conversion

--- Controllers/timeController.js
+++ Controllers/timeController.js
@@ -54,6 +54,8 @@ export const convertFromHours = async (req, res) => {
     return res.status(400).json({ error: "Invalid units" });
   }
 
+
+
   // Save to DB
   const conversion = new TimeModel({
     from: 'hours',
@@ -65,4 +67,41 @@ export const convertFromHours = async (req, res) => {
   await conversion.save();
 
   res.json({ from: 'hours', to, input: hours, result });
+};
+
+//Converting from Minutes to other units
+export const convertFromMinutes = async (req, res) => {
+  const { to, value } = req.body;
+
+  if (!to || value == null) {
+    return res.status(400).json({ error: "Invalid input" });
+  }
+
+  const minutes = parseFloat(value);
+  let result;
+
+  if (to === 'minutes') {
+    result = minutes; // no conversion needed
+  } else if (to === 'seconds') {
+    result = minutes * 60;
+  } else if (to === 'days') {
+    result = minutes / 1440;
+  } else if (to === 'hours') {
+    result = minutes / 60;
+  } else {
+    return res.status(400).json({ error: "Invalid units" });
+  }
+
+  
+  // Save to DB
+  const conversion = new TimeModel({
+    from: 'minutes',
+    to,
+    input: minutes,
+    result
+  });
+
+  await conversion.save();
+
+  res.json({ from: 'minutes', to, input: minutes, result });
 };
