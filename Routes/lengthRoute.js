4032579bf97f3f9774372f3c18cf68fb49536130
Michael-009 <adumichael56@gmail.com>
2025-05-14 20:44:46 -1200

    decimeter and meter logics

--- Routes/lengthRoute.js
+++ Routes/lengthRoute.js
@@ -1,10 +1,12 @@
 // Routes/lengthRoute.js
 import express from "express";
-import { convertLength, convertFromcentimeter } from "../Controllers/lengthController.js";
+import { convertLength, convertFromcentimeter, convertFrommeter, convertFromdecimeter } from "../Controllers/lengthController.js";
 
 const router = express.Router();
 
 // Define the length conversion route
 router.post('/from-mm', convertLength);
 router.post('/from-centimeter', convertFromcentimeter);
+router.post('/from-meter', convertFrommeter);
+router.post('/from-decimetre', convertFromdecimeter);
 export default router;
